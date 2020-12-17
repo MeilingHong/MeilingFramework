9590c89b06e0ab3fd062db1394f30300cc635f15
huangzhou <marisareimu@126.com>
2020-12-17 10:15:32 +0800

    基类方法进行说明；增加内存泄漏检测类；

--- component_base_activity/src/main/java/com/meiling/framework/base_activity/BaseActivity.java
+++ component_base_activity/src/main/java/com/meiling/framework/base_activity/BaseActivity.java
@@ -1,20 +1,14 @@
 package com.meiling.framework.base_activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
-import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 import com.meiling.framework.base_activity.util.statusbar.QMUIStatusBarHelper;
 
@@ -37,6 +31,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected boolean isBlockBackMode = false;
     protected boolean isDoubleBackExit = false;
     protected boolean isTranslucent = true;// 状态栏是否透明
+
+    protected int navigatorBarColor = -1;
 
 //    private Unbinder unbinder = null;
 
@@ -137,24 +133,33 @@ public abstract class BaseActivity extends AppCompatActivity {
         firstFocusCount = 1;
         // todo 设置屏幕显示方向
         setRequestedOrientation(isPortraitMode ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
+        /*
+    View.SYSTEM_UI_FLAG_VISIBLE：显示状态栏，Activity不全屏显示(恢复到有状态的正常情况)。
+    View.INVISIBLE：隐藏状态栏，同时Activity会伸展全屏显示。
+    View.SYSTEM_UI_FLAG_FULLSCREEN：Activity全屏显示，且状态栏被隐藏覆盖掉。
+    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN：Activity全屏显示，但状态栏不会被隐藏覆盖，状态栏依然可见，Activity顶端布局部分会被状态遮住。
+    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION：效果同View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    View.SYSTEM_UI_LAYOUT_FLAGS：效果同View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION：隐藏虚拟按键(导航栏)。有些手机会用虚拟按键来代替物理按键。
+    View.SYSTEM_UI_FLAG_LOW_PROFILE：状态栏显示处于低能显示状态(low profile模式)，状态栏上一些图标显示会被隐藏。
+         */
         // todo 设置屏幕是否全屏，设置状态栏字体颜色
         getWindow().getDecorView().setSystemUiVisibility(
-                isFullScreenMode && isStatusBarFontColorWhite ?
-                        (isTranslucent ? View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN :// todo 该参数会使得状态栏可见
-                                View.SYSTEM_UI_FLAG_FULLSCREEN) // todo 该参数会使得状态栏不可见-------------从而使得状态栏根据 Activity页面的需求进行变化
-                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE :
-                        isFullScreenMode && !isStatusBarFontColorWhite ? (isTranslucent ? View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN : View.SYSTEM_UI_FLAG_FULLSCREEN) | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR :
+                isFullScreenMode && isStatusBarFontColorWhite ? (View.SYSTEM_UI_FLAG_FULLSCREEN) | View.SYSTEM_UI_FLAG_LAYOUT_STABLE :
+                        isFullScreenMode && !isStatusBarFontColorWhite ? (View.SYSTEM_UI_FLAG_FULLSCREEN) | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR :
                                 !isFullScreenMode && isStatusBarFontColorWhite ? View.SYSTEM_UI_FLAG_LAYOUT_STABLE : View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        if (isFullScreenMode) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//
-        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         // 状态栏（以上几行代码必须，参考setStatusBarColor|setNavigationBarColor方法源码）
-        // todo 设置底部导航栏的颜色
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+//        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
-        setStatusFontColor(isStatusBarFontColorWhite);
+        setStatusFontColor(isStatusBarFontColorWhite);// 实际上，仅调用这一个方法就行了，上面的方法不需要再额外进行调用
+
+        // todo 设置底部导航栏的颜色
+        if (navigatorBarColor != -1) {
+            getWindow().setNavigationBarColor(navigatorBarColor);// 可根据需要自定义
+        }
 
         // todo 是否保持该页面的屏幕处于常亮状态
         if (isKeepScreenMode) {
